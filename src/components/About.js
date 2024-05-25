951bf17f6662db256090ee428ee2c5a1a85716af
Sumeet <sumeetp987@gmail.com>
2024-05-25 19:24:42 +0530

    made changes to about us

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,11 @@
 const About = () => {
     return(
         <div className="about">
-            <h2>Hello, we are food-web.</h2>
+            <h2>Hello, we are FoodWeb.</h2>
             <h3>Order your tasty food online...<i className="fa-solid fa-burger"></i></h3>
+            <a href="https://github.com/Sumeettt?tab=repositories" target="blank">
+            <i className="fa-brands fa-github github-logo"></i>
+            </a>
         </div>
     )
 }
